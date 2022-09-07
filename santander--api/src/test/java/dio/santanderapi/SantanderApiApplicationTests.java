bc20ad07f5a042d999d360983402322091087969
carlos738 <carlosasl135@hotmail.com>
2022-09-07 02:25:59 -0300

    SANTANDER-DEV-WEEK

---
+++ santander--api/src/test/java/dio/santanderapi/SantanderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.santanderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
